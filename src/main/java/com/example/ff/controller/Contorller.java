76604b6c2db7c0c62e8939b0186ba861a014ed34
JCHPJP <2858829498@qq.com>
2025-07-01 15:47:00 +0800

    修改依赖

--- src/main/java/com/example/ff/controller/Contorller.java
+++ src/main/java/com/example/ff/controller/Contorller.java
@@ -3,16 +3,11 @@ package com.example.ff.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-
-import javax.sql.DataSource;
 import java.util.HashMap;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class Contorller {
-    @Autowired
-    private DataSource dataSource ;
-
     @Value("${aaa.url}")
     private String uuu ;
 
@@ -20,7 +15,6 @@ public class Contorller {
     public String hello(){
         HashMap<String, Integer> mp = new HashMap<>() ;
         mp.put("12345",123456878) ;
-        System.out.println(dataSource);
 //        System.out.println("dataSourse" + dataSource);
         System.out.println(uuu);
         return uuu;
